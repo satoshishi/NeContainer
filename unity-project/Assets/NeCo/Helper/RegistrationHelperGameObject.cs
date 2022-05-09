41a258a8b387c3458b6204fdeef4d6ff45a199ec
satoshishi <s9r6nbij9@gmail.com>
2022-05-09 11:20:04 +0900

    registration helperの基底クラスをリファクタリング

--- unity-project/Assets/NeCo/Helper/RegistrationHelperGameObject.cs
+++ unity-project/Assets/NeCo/Helper/RegistrationHelperGameObject.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace NeCo.Helper
-{    
-    [DisallowMultipleComponent]    
-    public abstract class RegistrationHelperBase : MonoBehaviour
+{
+    [DisallowMultipleComponent]
+    public abstract class RegistrationHelperGameObject : MonoBehaviour, IRegistrationHelper
     {
         public abstract INeCoResolver RegistrationAndBuild();
-        
+
         public abstract INeCoBuilder Registration(INeCoBuilder container = default);
     }
 }
---
+++ unity-project/Assets/NeCo/Helper/RegistrationHelperScriptableObject.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NeCo.Helper
+{
+
+    [CreateAssetMenu(fileName = "Write Name", menuName = "NeCo/RegistrationHelperScriptableObject")]
+    public class RegistrationHelperScriptableObject : ScriptableObject,IRegistrationHelper
+    {
+        [SerializeField]
+        private RegistrationHelperGameObject[] m_helpers;
+
+        public INeCoResolver RegistrationAndBuild()
+        {
+            var container = NeCoUtilities.Create();
+
+            foreach (var helper in m_helpers)
+            {
+                container = helper.Registration(container);
+            }
+
+            return container.Build();
+        }       
+        public INeCoBuilder Registration(INeCoBuilder container = default)
+        {
+            foreach (var helper in m_helpers)
+            {
+                container = helper.Registration(container);
+            }
+
+            return container;            
+        }
+    }
+}
